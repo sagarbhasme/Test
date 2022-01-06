a2623d6b887457410f563d3ee64a8ca320c551be
sagarbhasme <sagarbhasme29@gmail.com>
2022-01-06 13:29:24 +0530

    testing in progress

--- src/newTest/Test.java
+++ src/newTest/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hey Sagar test the scenario, please check");
+System.out.println("Hey Sagar test the scenario, please check the scenario");
 	}
 
 }
